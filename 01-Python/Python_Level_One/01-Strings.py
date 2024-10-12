1b0a0e4f57f77f05440a91c76ab1b6394505f4f3
arif wicaksono septyanto <codelearnid@gmail.com>
2024-10-13 00:42:36 +0800

    update materi

--- 01-Python/Python_Level_One/01-Strings.py
+++ 01-Python/Python_Level_One/01-Strings.py
@@ -2,247 +2,243 @@
 ###### Strings ###############
 ##############################
 
-# Strings are used in Python to record text information, such as name. Strings
-# in Python are actually a *sequence*, which basically means Python keeps track
-# of every element in the string as a sequence. For example, Python understands
-# the string "hello' to be a sequence of letters in a specific order. This means
-# we will be able to use indexing to grab particular letters (like the first
-# letter, or the last letter).
+# Strings digunakan dalam Python untuk merekam informasi teks, seperti nama. Strings
+# dalam Python sebenarnya adalah sebuah *urutan*, yang berarti Python melacak
+# setiap elemen dalam string sebagai urutan tertentu. Misalnya, Python mengerti
+# string "hello" sebagai urutan huruf dalam urutan tertentu. Ini berarti
+# kita akan dapat menggunakan indeks untuk mengambil huruf tertentu (seperti huruf pertama,
+# atau huruf terakhir).
 #
-# This idea of a sequence is an important one in Python and we will touch
-# upon it later on in the future.
+# Ide ini tentang urutan adalah salah satu yang penting dalam Python dan kita akan membahas
+# lebih lanjut tentang ini di masa depan.
 #
-# In this lecture we'll learn about the following:
+# Dalam kuliah ini kita akan belajar tentang:
 #
-#     1.) Creating Strings
-#     2.) Printing Strings
-#     3.) String Indexing and Slicing
-#     4.) String Properties
-#     5.) String Methods
-#     6.) Print Formatting
+#     1.) Membuat Strings
+#     2.) Mencetak Strings
+#     3.) Indeks dan Slicing Strings
+#     4.) Properti Strings
+#     5.) Metode Strings
+#     6.) Format Pencetakan
 
-# Creating a String
-# To create a string in Python you need to use either
-# single quotes or double quotes. For example:
+# Membuat String
+# Untuk membuat string dalam Python, Anda perlu menggunakan tanda kutip tunggal atau ganda. Contoh:
 
-# Single word
+# Kata tunggal
 'hello'
 
-# Entire phrase
+# Frase lengkap
 'This is also a string'
 
-# We can also use double quote
-"String built with double quotes"
+# Kita juga dapat menggunakan tanda kutip ganda
+"String dibangun dengan tanda kutip ganda"
 
 
-# Be careful with quotes!
-' I'm using single quotes, but will create an error'
+# Berhati-hati dengan tanda kutip!
+' I\'m using single quotes, but will create an error'
 
 
-# The reason for the error above is because the single quote in I'm stopped the
-# string. You can use combinations of double and single quotes to get the complete statement.
+# Alasan untuk kesalahan di atas adalah karena tanda kutip tunggal dalam I'm menghentikan
+# string. Anda dapat menggunakan kombinasi tanda kutip tunggal dan ganda untuk mendapatkan pernyataan lengkap.
 
-"Now I'm ready to use the single quotes inside a string!"
+"Now I\'m ready to use the single quotes inside a string!"
 
 
-# Now let's learn about printing strings!
+# Sekarang mari kita belajar tentang mencetak strings!
 
-# ## Printing a String
+# ## Mencetak String
 #
-# Using the REPL with just a string in a cell will automatically output
-# strings, but the correct way to display strings in your output is by using a print function.
+# Menggunakan REPL dengan hanya string dalam sel akan secara otomatis menghasilkan
+# strings, tetapi cara yang benar untuk menampilkan strings dalam output Anda adalah dengan menggunakan fungsi print.
 
-# We can simply declare a string
+# Kita dapat menyatakan string
 'Hello World'
 
-# note that we can't output multiple strings this way
+# perhatikan bahwa kita tidak dapat menghasilkan beberapa strings dengan cara ini
 'Hello World 1'
 'Hello World 2'
 
 
-# We can use a print statement to print a string.
+# Kita dapat menggunakan pernyataan print untuk mencetak string.
 
 print('Hello World 1')
 print('Hello World 2')
-print('Use \n to print a new line')
+print('Gunakan \n untuk mencetak baris baru')
 print('\n')
-print('See what I mean?')
+print('Lihat apa yang saya maksud?')
 
 
-# String Basics
+# Dasar Strings
 
-# We can also use a function called len() to check the length of a string!
+# Kita juga dapat menggunakan fungsi yang disebut len() untuk memeriksa panjang string!
 
 len('Hello World')
 
 
-# ## String Indexing
-# We know strings are a sequence, which means Python can use indexes to call
-# parts of the sequence. Let's learn how this works.
+# ## Indeks Strings
+# Kita tahu bahwa strings adalah urutan, yang berarti Python dapat menggunakan indeks untuk memanggil
+# bagian dari urutan. Mari kita belajar bagaimana ini bekerja.
 #
-# In Python, we use brackets [] after an object to call its index. We should
-# also note that indexing starts at 0 for Python. Let's create a new object
-# called s and the walk through a few examples of indexing.
+# Dalam Python, kita menggunakan kurung [] setelah objek untuk memanggil indeksnya. Kita juga harus
+# mencatat bahwa indeks dimulai dari 0 untuk Python. Mari kita membuat objek baru
+# yang disebut s dan berjalan melalui beberapa contoh indeks.
 
-# Assign s as a string
+# Tetapkan s sebagai string
 s = 'Hello World'
 
 #Check
 s
 
-# Print the object
+# Cetak objek
 print(s)
 
 
-# Let's start indexing!
+# Mari kita mulai indeksing!
 
-# Show first element (in this case a letter)
+# Tampilkan elemen pertama (dalam hal ini huruf)
 s[0]
 
-# Next element
+# Elemen berikutnya
 s[1]
 
-# Next Element
+# Elemen berikutnya
 s[2]
 
 
-# We can use a : to perform *slicing* which grabs everything
-# up to a designated point. For example:
+# Kita dapat menggunakan : untuk melakukan *slicing* yang mengambil semuanya
+# hingga titik yang ditentukan. Misalnya:
 
-# Grab everything past the first term all the way to the length of s which is len(s)
+# Ambil semuanya setelah istilah pertama hingga panjang s yang adalah len(s)
 s[1:]
 
 
-# Note that there is no change to the original s
+# Perhatikan bahwa tidak ada perubahan pada s asli
 s
 
-# Grab everything UP TO the 3rd index
+# Ambil semuanya HINGGA indeks ke-3
 s[:3]
 
 
-# Note the above slicing. Here we're telling Python to grab everything from
-# 0 up to 3. It doesn't include the 3rd index. You'll notice this a lot in
-# Python, where statements and are usually in the context of "up to, but not including".
+# Perhatikan slicing di atas. Di sini kita memberitahu Python untuk mengambil semuanya dari
+# 0 hingga 3. Tidak termasuk indeks ke-3. Anda akan melihat ini banyak di
+# Python, di mana pernyataan dan biasanya dalam konteks "hingga, tapi tidak termasuk".
 
-#Everything
+# Semua
 s[:]
 
 
-# We can also use negative indexing to go backwards.
-# Last letter (one index behind 0 so it loops back around)
+# Kita juga dapat menggunakan indeks negatif untuk bergerak mundur.
+# Huruf terakhir (satu indeks di belakang 0 sehingga berputar kembali)
 s[-1]
 
-# Grab everything but the last letter
+# Ambil semuanya tapi huruf terakhir
 s[:-1]
 
 
-# We can also use index and slice notation to grab elements of a sequence by a
-# specified step size (the default is 1). For instance we can use two colons in
-# a row and then a number specifying the frequency to grab elements. For example:
+# Kita juga dapat menggunakan notasi indeks dan slice untuk mengambil elemen dari urutan dengan ukuran langkah tertentu (ukuran langkah default adalah 1). Misalnya:
 
-# Grab everything, but go in steps size of 1
+# Ambil semuanya, tapi pergi dalam ukuran langkah 1
 s[::1]
 
-# Grab everything, but go in step sizes of 2
+# Ambil semuanya, tapi pergi dalam ukuran langkah 2
 s[::2]
 
-# We can use this to print a string backwards
+# Kita dapat menggunakan ini untuk mencetak string terbalik
 s[::-1]
 
 
-# ## String Properties
-# Its important to note that strings have an important property known as
-# immutability. This means that once a string is created, the elements within
-# it can not be changed or replaced. For example:
+# ## Properti Strings
+# Penting untuk dicatat bahwa strings memiliki properti yang penting yaitu
+# keabadian. Ini berarti bahwa sekali string dibuat, elemen dalamnya tidak dapat diubah atau diganti. Misalnya:
 
 s
 
-# Let's try to change the first letter to 'x'
+# Mari kita coba mengubah huruf pertama menjadi 'x'
 s[0] = 'x'
 
 
-# Notice how the error tells us directly what we can't do,
-# change the item assignment!
+# Perhatikan bagaimana kesalahan itu memberitahu kita langsung apa yang tidak dapat kita lakukan,
+# mengubah penugasan item!
 #
-# Something we can do is concatenate strings!
+# Sesuatu yang dapat kita lakukan adalah menggabungkan strings!
 s
 
-# Concatenate strings!
+# Menggabungkan strings!
 s + ' concatenate me!'
 
-# We can reassign s completely though!
+# Kita dapat mengubah s sepenuhnya meskipun!
 s = s + ' concatenate me!'
 
 print(s)
 
-# We can use the multiplication symbol to create repetition!
+# Kita dapat menggunakan simbol perkalian untuk membuat pengulangan!
 
 letter = 'z'
 
 letter*10
 
 
-# ## Basic Built-in String methods
+# ## Metode Strings Dasar
 #
-# Objects in Python usually have built-in methods. These methods are functions
-# inside the object (we will learn about these in much more depth later) that
-# can perform actions or commands on the object itself.
+# Objek dalam Python biasanya memiliki metode bawaan. Metode-metode ini adalah fungsi
+# di dalam objek (kita akan belajar lebih lanjut tentang ini nanti) yang dapat melakukan
+# tindakan atau perintah pada objek itu sendiri.
 #
-# We call methods with a period and then the method name. Methods are in the form:
+# Kita memanggil metode dengan titik dan kemudian nama metode. Metode-metode ini dalam bentuk:
 #
 # object.method(parameters)
 #
-# Where parameters are extra arguments we can pass into the method.
-# Don't worry if the details don't make 100% sense right now. Later on we will
-# be creating our own objects and functions!
+# Di mana parameters adalah argumen tambahan yang kita dapat lewatkan ke metode.
+# Jangan khawatir jika detailnya tidak 100% masuk akal sekarang. Nanti kita akan
+# membuat objek dan fungsi kita sendiri!
 #
-# Here are some examples of built-in methods in strings:
+# Berikut adalah beberapa contoh metode bawaan dalam strings:
 
-# Upper Case a string
+# Huruf Besar sebuah string
 s.upper()
 
-# Lower case
+# Huruf Kecil
 s.lower()
 
-# Split a string by blank space (this is the default)
+# Pisahkan string dengan ruang kosong (ini adalah default)
 s.split()
 
-# Split by a specific element (doesn't include the element that was split on)
+# Pisahkan oleh elemen tertentu (tidak termasuk elemen yang dipisahkan)
 s.split('W')
 
-# There are many more methods than the ones covered here.
+# Ada banyak metode lainnya yang tidak ditutupi di sini.
 
 ########################
-### Print Formatting ###
+### Format Pencetakan ###
 ########################
 
-# We can use the .format() method to add formatted objects to printed string statements.
+# Kita dapat menggunakan metode .format() untuk menambahkan objek yang diformat ke pernyataan cetak string.
 #
-# The easiest way to show this is through an example:
+# Cara termudah untuk menunjukkan ini adalah melalui contoh:
 
 'Insert another string with curly brackets: {}'.format('The inserted string')
 
-# Using the string .format() method
-# The best way to format objects into your strings for print statements is using
-# the format method. The syntax is:
+# Menggunakan metode string .format()
+# Cara terbaik untuk memformat objek ke dalam pernyataan cetak Anda adalah menggunakan
+# metode format. Sintaksnya adalah:
 #
 #  'String here {var1} then also {var2}'.format(var1='something1',var2='something2')
 #
-# Lets see some examples:
+# Mari kita lihat beberapa contoh:
 
 
 print('This is a string with an {p}'.format(p='insert'))
 
-# Multiple times:
+# Beberapa kali:
 print('One: {p}, Two: {p}, Three: {p}'.format(p='Hi!'))
 
 
-# Several Objects:
+# Beberapa Objek:
 print('Object 1: {a}, Object 2: {b}, Object 3: {c}'.format(a=1,b='two',c=12.3))
 
 
-# That is the basics of string formatting!
-# Often we'll use the simpler form with f-string literals (MUST USE PY 3.6 FOR THIS)
+# Itu dasar dari format string!
+# Seringkali kita akan menggunakan bentuk yang lebih sederhana dengan literal f-string (HARUS GUNAKAN PY 3.6 UNTUK INI)
 username = "Jose"
 color = "Blue"
 print(f"The name is {username} and color is {color}")
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Hands-On-Flask
 
 Python version : 3.12.5
+
+Referensi :
+
+[HTML elements reference](https://developer.mozilla.org/en-US/docs/Web/HTML/Element "HTML elements reference")
+
